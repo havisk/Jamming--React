b0c9835c4001d4bd4698777ec5978d9ff63747a0
Kool Havis <havisk@gmail.com>
2018-05-01 22:38:17 -0400

    added components and css for app.css

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+
+class Playlist extends Component {
+  render(){
+    return(<div>playlist</div>)
+  }
+}
+
+export default Playlist;
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+
+class SearchResults extends Component {
+  render(){
+    return(<div>search results</div>)
+  }
+}
+
+export default SearchResults;
